f465899d3c55f8e538b7b223916c4a64c792c3f6
Andriy Dumchykov <Andriy_Dumchykov@epam.com>
2017-05-16 15:16:24 +0300

    improved Utils.

--- src/main/java/com/my/d4/numgen/Utils.java
+++ src/main/java/com/my/d4/numgen/Utils.java
@@ -1,15 +1,13 @@
 package com.my.d4.numgen;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by dumin on 5/12/17.
  */
 public class Utils {
     public static int getDelay(){
-        int newDigit = new Random().nextInt(5-1+1);
-//        System.out.println(String.format("new digit = %04d",newDigit));
-        return newDigit;
+        return ThreadLocalRandom.current().nextInt(5)+1;
     }
-
 }
